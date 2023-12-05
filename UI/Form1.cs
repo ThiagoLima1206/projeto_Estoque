57311b8b4217b642134c756fcb1013a2d6c9d2ac
Vault-Obsidian-ThiagoLima <thiagolima120603@outlook.com>
2023-12-04 21:16:37 -0300

    mais um teste

--- UI/Form1.cs
+++ UI/Form1.cs
@@ -20,6 +20,7 @@ namespace UI
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Teste do gitHub");
+            MessageBox.Show("Teste da Branch");
         }
     }
 }

680d9417b1fc396889e3245dd00433e15e1f1f20
Vault-Obsidian-ThiagoLima <thiagolima120603@outlook.com>
2023-12-05 21:29:43 -0300

    - Comentei o método Todos() e o método SetProperty()

--- UI/Form1.Designer.cs
+++ UI/Form1.Designer.cs
@@ -36,13 +36,18 @@ namespace UI
             this.txtNome = new System.Windows.Forms.TextBox();
             this.txtDescricao = new System.Windows.Forms.TextBox();
             this.btnSalvar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.IdProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(266, 104);
+            this.label1.Location = new System.Drawing.Point(283, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 28);
             this.label1.TabIndex = 0;
@@ -52,7 +57,7 @@ namespace UI
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(237, 139);
+            this.label2.Location = new System.Drawing.Point(254, 63);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(73, 28);
             this.label2.TabIndex = 1;
@@ -62,7 +67,7 @@ namespace UI
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(207, 184);
+            this.label3.Location = new System.Drawing.Point(224, 108);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(103, 28);
             this.label3.TabIndex = 1;
@@ -70,28 +75,28 @@ namespace UI
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(316, 105);
+            this.txtId.Location = new System.Drawing.Point(333, 29);
             this.txtId.Name = "txtId";
             this.txtId.Size = new System.Drawing.Size(198, 27);
             this.txtId.TabIndex = 2;
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(316, 143);
+            this.txtNome.Location = new System.Drawing.Point(333, 67);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(198, 27);
             this.txtNome.TabIndex = 3;
             // 
             // txtDescricao
             // 
-            this.txtDescricao.Location = new System.Drawing.Point(316, 188);
+            this.txtDescricao.Location = new System.Drawing.Point(333, 112);
             this.txtDescricao.Name = "txtDescricao";
             this.txtDescricao.Size = new System.Drawing.Size(198, 27);
             this.txtDescricao.TabIndex = 4;
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(316, 239);
+            this.btnSalvar.Location = new System.Drawing.Point(333, 163);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(198, 29);
             this.btnSalvar.TabIndex = 5;
@@ -99,11 +104,47 @@ namespace UI
             this.btnSalvar.UseVisualStyleBackColor = true;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdProd,
+            this.Nome,
+            this.Descricao});
+            this.dataGridView1.Location = new System.Drawing.Point(201, 224);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(429, 188);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // IdProd
+            // 
+            this.IdProd.HeaderText = "IdProd";
+            this.IdProd.MinimumWidth = 6;
+            this.IdProd.Name = "IdProd";
+            this.IdProd.Width = 125;
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.MinimumWidth = 6;
+            this.Nome.Name = "Nome";
+            this.Nome.Width = 125;
+            // 
+            // Descricao
+            // 
+            this.Descricao.HeaderText = "Descricao";
+            this.Descricao.MinimumWidth = 6;
+            this.Descricao.Name = "Descricao";
+            this.Descricao.Width = 125;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.txtDescricao);
             this.Controls.Add(this.txtNome);
@@ -114,6 +155,7 @@ namespace UI
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,6 +170,10 @@ namespace UI
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.TextBox txtDescricao;
         private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdProd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descricao;
     }
 }
 
